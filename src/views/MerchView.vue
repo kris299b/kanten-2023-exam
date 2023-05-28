8f8b7b33b5c44ebf1abce9d3854673ac3faa2bff
stepag01 <stepag01@easv365.dk>
2023-05-28 20:06:59 +0200

    en masse tilpasninger og ændringer

--- src/views/MerchView.vue
+++ src/views/MerchView.vue
@@ -7,56 +7,23 @@
 </div>
 
 
-<div class="flex flex-row justify-center bg-black">
-    <div class="max-w-sm lg:w-80 shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <img class="h-80 mx-auto" src="../assets/t-shirt.png" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                SE T-SHIRTS
-                <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-        </div>
-    </div>
-
-    <div class="max-w-sm lg:w-80 bg-black shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <img class="h-80 mx-auto" src="../assets/hoodie.png" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                SE HOODIES
-                <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-        </div>
-    </div>
-
-    <div class="max-w-sm lg:w-80 bg-black shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <img class="h-80 mx-auto" src="../assets/sweatshirt.png" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="#">
-                <h5 class="mb-2 text-base md:text-base lg:text-xl font-bold tracking-tight text-gray-900 dark:text-white">HOODIES</h5>
-            </a>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                SE SWEATSHIRTS
-                <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-        </div>
+<div class="boks2">
+    <div class="inner-boks2">
+        <img id="t-shirt" src="../assets/t-shirt.png" alt="">
+        <img id="hoodie" src="../assets/hoodie-kopi.png" alt="">
+        <img id="sweatshirt" src="../assets/sweatshirt-kopi.png" alt="">
     </div>
 </div>
-
 </template>
 
-<script setup>
 
+<script setup>
 </script>
+
 
 <style scoped>
 .boks1 {
-    background-color: rgb(0, 179, 27);
+    background-color: rgb(0, 0, 0);
     height: 250px;
 }
 
@@ -83,6 +50,41 @@ h1 {
 }
 
 .boks2 {
-    height: 500px;
+    background-color: rgb(0, 0, 0);
+    height: 450px;
+}
+
+.inner-boks2 {
+    background-color: rgb(0, 0, 0);
+    height: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#t-shirt{
+    height: 90%;
+    margin-right: 40px;
+}
+
+#hoodie{
+    height: 90%;
+    margin-right: 40px;
+}
+
+#sweatshirt{
+    height: 90%;
+}
+
+
+.inner-boks3 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.button {
+  background-color: rgb(0, 0, 0);
+  color: #e6e6e6;
+  border: 2px solid #e6e6e6;
 }
 </style>
