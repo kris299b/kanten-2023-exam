f6190dc3b8ff9cf3b86dbf2bc1b9a6fde7042288
stepag01 <stepag01@easv365.dk>
2023-05-30 23:19:17 +0200

    Update FrivilligView.vue

--- src/views/FrivilligView.vue
+++ src/views/FrivilligView.vue
@@ -24,21 +24,21 @@
 
 
   <section class="bg-white dark:bg-black">
-  <div class="py-8 md:pt-32 lg: px-4 mx-auto max-w-screen-md">
-      <h2 class="mb-4 text-base md:text-base lg:text-3xl tracking-tight 0.1em; font-bold font-sans text-center text-gray-900 dark:text-white">SKAL DU VÆRE MED PÅ HOLDET?</h2>
+  <div class="py-8 md:pt-32 lg:pt-16 px-4 mx-auto max-w-screen-md">
+      <h2 class="mb-4 text-base md:text-xl lg:text-3xl tracking-tight 0.1em; font-bold font-sans text-center text-gray-900 dark:text-white">SKAL DU VÆRE MED PÅ HOLDET?</h2>
       <p class="mb-8 lg:mb-16 font-light font-serif text-center text-gray-500 dark:text-gray-400 text-sm md:text-base lg:text-base">Kunne du tænke dig at blive en del af holdet og indgå i et kæmpe fællesskab, hvor alle er velkommen og kan føle sig inkluderet? Så tøv endelig ikke med at udfylde formularen herunder. Vi glæder os til at høre fra dig.</p>
       <form action="#" class="space-y-8">
           <div>
-              <label for="email" class="block mb-2 text-xs font-semibold text-gray-900 dark:text-gray-300">Navn</label>
-              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Dit navn..." required>
+              <label for="email" class="block mb-2 text-xs md:text-base font-semibold text-gray-900 dark:text-gray-300">Navn</label>
+              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs md:text-base focus:ring-primary-500 focus:border-primary-500 block w-full lg:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Dit navn..." required>
           </div>
           <div>
-              <label for="subject" class="block mb-2 text-xs font-semibold text-gray-900 dark:text-gray-300">Email</label>
-              <input type="text" id="subject" class="block p-3 w-full text-xs text-gray-900 bg-gray-50 border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Din email..." required>
+              <label for="subject" class="block mb-2 text-xs md:text-base font-semibold text-gray-900 dark:text-gray-300">Email</label>
+              <input type="text" id="subject" class="block p-3 w-full text-xs md:text-base text-gray-900 bg-gray-50 border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Din email..." required>
           </div>
           <div class="sm:col-span-2">
-              <label for="message" class="block mb-2 text-xs font-semibold text-gray-900 dark:text-gray-300">Skriv lidt om dig selv</label>
-              <textarea id="message" rows="6" class="block p-2.5 w-full text-xs text-gray-900 bg-gray-50 shadow-sm border border-gray-300 focus:ring-slate-50 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Fortæl om dig selv..."></textarea>
+              <label for="message" class="block mb-2 text-xs md:text-base font-semibold text-gray-900 dark:text-gray-300">Skriv lidt om dig selv</label>
+              <textarea id="message" rows="6" class="block p-2.5 w-full text-xs md:text-base text-gray-900 bg-gray-50 shadow-sm border border-gray-300 focus:ring-slate-50 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Fortæl om dig selv..."></textarea>
           </div>
           <button type="submit" class="py-1 px-5 text-sm md:text-base lg:text-base font-semibold text-center text-white bg-black border-2 border-slate-300 sm:w-fit hover:bg-orange-400 hover:text-black hover:font-semibold hover:border-orange-400 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">SEND</button>
       </form>
