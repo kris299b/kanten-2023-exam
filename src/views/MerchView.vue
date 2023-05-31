ef23b49681a8fa70579c568c7327285ff4bf8b8f
stepag01 <stepag01@easv365.dk>
2023-05-31 21:44:36 +0200

    en heeeeel masse ændringer af responsive design.....

--- src/views/MerchView.vue
+++ src/views/MerchView.vue
@@ -1,4 +1,5 @@
 <template>
+
 <div class="boks1">
     <div class="inner-boks1">
         <h1>MERCHANDISE</h1>
@@ -129,77 +130,64 @@ h1 {
 
 
 
-/* @media (width < 640px) {
+
+/* Custom, iPhone Retina */ 
+@media only screen and (min-width : 320px) {
     .boks1 {
-        background-color: rgb(0, 0, 0);
-        height: 220px;
+        height: 200px;
     }
 
     .inner-boks1 {
-        background-color: #000000;
         height: 200px;
         padding-top: 30px;
         width: 90%;
     }
 
     h1 {
-        font-size: 22px;
-        letter-spacing: 10px;
-        padding-bottom: 10px;
+        font-size: 18px;
+        letter-spacing: 6px;
+        padding-bottom: 15px;
     }
 
     #paragraf {
-        font-size: 11px;
+        font-size: 12px;
     }
-
     .boks2 {
-        background-color: rgb(0, 0, 0);
         height: 1300px;
         width: 100%;
         display: grid;
         padding-bottom: 40px;
     }
-
     .inner-boks2 {
-        background-color: rgb(0, 0, 0);
         height: 400px;
         width: 100%;
-        margin-bottom: 30px;
     }
 
     #t-shirt{
         height: 80%;
     }
-
     .inner-boks3 {
-        background-color: rgb(0, 0, 0);
         height: 400px;
         width: 100%;
-        margin-bottom: 30px;
     }
 
     #hoodie{
         height: 80%;
     }
-
     .inner-boks4 {
-        background-color: rgb(0, 0, 0);
         height: 400px;
         width: 100%;
-        text-align: center;
     }
 
     #sweatshirt{
         height: 80%;
     }
-
-
     .inner-boks3 {
-        background-color: rgb(0, 0, 0);
         height: 400px;
     }
     .button {
-        margin-top: 15px;
+        margin-top: 20px;
+        margin-bottom: 40px;
         padding: 5px;
         font-size: 15px;
     }
@@ -207,168 +195,258 @@ h1 {
 
 
 
-
-@media (min-width: 768px) {
-
-.inner-boks1 {
-    width: 70%;
-}
-
-h1 {
-    font-size: 30px;
-}
-
-#paragraf {
-    font-size: 16px;
-}
-
-#t-shirt{
-    height: 80%;
-}
-
-#hoodie{
-    height: 80%;
-}
-
-#sweatshirt{
-    height: 80%;
-}
-.button {
-  margin-top: 20px;
-}
-}
-
-
-
-@media (min-width: 992px) {
-  .boks1 {
-    background-color: rgb(0, 0, 0);
-    height: 250px;
-}
-
-.inner-boks1 {
-    background-color: rgb(0, 0, 0);
-    height: 200px;
-    padding-top: 30px;
-    width: 60%;
-    margin: auto;
-    text-align: center;
-}
-
-h1 {
-    font-size: 45px;
-    color: #e7e7e7;
-    font-weight: 600;
-    letter-spacing: 15px;
-    padding-bottom: 20px;
-}
-
-#paragraf {
-    font-size: 16px;
-    color: #e7e7e7;
-    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-}
-
-.boks2 {
-    background-color: rgb(0, 0, 0);
-    height: 500px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-bottom: 40px;
-}
-
-.inner-boks2 {
-    background-color: rgb(0, 0, 0);
-    height: 400px;
-    width: 33.3%;
-    margin: auto;
-    text-align: center;
-}
-
-#t-shirt{
-    height: 90%;
-    margin: auto;
-}
-
-.inner-boks3 {
-    background-color: rgb(0, 0, 0);
-    height: 400px;
-    width: 33.3%;
-    text-align: center;
-}
-
-#hoodie{
-    height: 90%;
-    margin: auto;
-}
-
-.inner-boks4 {
-    background-color: rgb(0, 0, 0);
-    height: 400px;
-    width: 33.3%;
-    text-align: center;
-}
-
-#sweatshirt{
-    height: 90%;
-    margin: auto;
-}
-
-
-.inner-boks3 {
-    background-color: rgb(0, 0, 0);
-    height: 400px;
-}
-.button {
-  background-color: rgb(0, 0, 0);
-  color: #e6e6e6;
-  font-weight: 600;
-  border: 2px solid #e6e6e6;
-  margin-top: 10px;
-  padding: 5px;
-}
-
-.button:hover {
-    background-color: #fb923c;
-    border-color: #fb923c;
-    color: black;
-}
-} */
-
-
-
-/* Custom, iPhone Retina */ 
-@media only screen and (min-width : 320px) {
-
-}
-
-
-
 /* Extra Small Devices, Phones */ 
 @media only screen and (min-width : 480px) {
+    .boks1 {
+        height: 200px;
+    }
 
+    .inner-boks1 {
+        height: 200px;
+        padding-top: 30px;
+        width: 70%;
+    }
+
+    h1 {
+        font-size: 18px;
+        letter-spacing: 10px;
+        padding-bottom: 10px;
+    }
+
+    #paragraf {
+        font-size: 12px;
+    }
+    .boks2 {
+        height: 1300px;
+        width: 100%;
+        display: grid;
+        padding-bottom: 40px;
+    }
+    .inner-boks2 {
+        height: 400px;
+        width: 100%;
+    }
+
+    #t-shirt{
+        height: 80%;
+    }
+    .inner-boks3 {
+        height: 400px;
+        width: 100%;
+    }
+
+    #hoodie{
+        height: 80%;
+    }
+    .inner-boks4 {
+        height: 400px;
+        width: 100%;
+    }
+
+    #sweatshirt{
+        height: 80%;
+    }
+    .inner-boks3 {
+        height: 400px;
+    }
+    .button {
+        margin-top: 20px;
+        margin-bottom: 40px;
+        padding: 5px;
+        font-size: 15px;
+    }
 }
 
 
 
 /* Small Devices, Tablets */
 @media only screen and (min-width : 768px) {
+    .boks1 {
+        height: 200px;
+    }
 
+    .inner-boks1 {
+        height: 200px;
+        padding-top: 30px;
+        width: 70%;
+    }
+
+    h1 {
+        font-size: 30px;
+        letter-spacing: 10px;
+        padding-bottom: 20px;
+    }
+
+    #paragraf {
+        font-size: 13px;
+    }
+    .boks2 {
+        height: 500px;
+        width: 100%;
+        display: flex;
+        padding-bottom: 40px;
+    }
+    .inner-boks2 {
+        height: 400px;
+        width: 33.3%;
+    }
+
+    #t-shirt{
+        height: 80%;
+    }
+    .inner-boks3 {
+        height: 400px;
+        width: 33.3%;
+    }
+
+    #hoodie{
+        height: 80%;
+    }
+    .inner-boks4 {
+        height: 400px;
+        width: 33.3%;
+    }
+
+    #sweatshirt{
+        height: 80%;
+    }
+    .inner-boks3 {
+        height: 400px;
+    }
+    .button {
+        margin-top: 20px;
+        padding: 5px;
+    }
 }
 
 
 
 /* Medium Devices, Desktops */
-@media only screen and (min-width : 992px) {
+    @media only screen and (min-width : 992px) {
+    .boks1 {
+        height: 250px;
+    }
 
+    .inner-boks1 {
+        height: 200px;
+        padding-top: 30px;
+        width: 60%;
+    }
+
+    h1 {
+        font-size: 45px;
+        letter-spacing: 15px;
+        padding-bottom: 20px;
+    }
+
+    #paragraf {
+        font-size: 16px;
+    }
+    .boks2 {
+        height: 500px;
+        width: 100%;
+        display: flex;
+        padding-bottom: 40px;
+    }
+    .inner-boks2 {
+        height: 400px;
+        width: 33.3%;
+    }
+
+    #t-shirt{
+        height: 90%;
+    }
+    .inner-boks3 {
+        height: 400px;
+        width: 33.3%;
+    }
+
+    #hoodie{
+        height: 90%;
+    }
+    .inner-boks4 {
+        height: 400px;
+        width: 33.3%;
+    }
+
+    #sweatshirt{
+        height: 90%;
+    }
+    .inner-boks3 {
+        height: 400px;
+    }
+    .button {
+        margin-top: 10px;
+        padding: 5px;
+    }
 }
 
 
 
 /* Large Devices, Wide Screens */
 @media only screen and (min-width : 1200px) {
-  
+    .boks1 {
+        height: 250px;
+    }
+
+    .inner-boks1 {
+        height: 200px;
+        padding-top: 30px;
+        width: 70%;
+    }
+
+    h1 {
+        font-size: 45px;
+        font-weight: 600;
+        letter-spacing: 15px;
+        padding-bottom: 20px;
+    }
+
+    #paragraf {
+        font-size: 16px;
+    }
+
+    .boks2 {
+        height: 500px;
+        width: 100%;
+        display: flex;
+        padding-bottom: 40px;
+    }
+
+    .inner-boks2 {
+        height: 400px;
+        width: 33.3%;
+    }
+
+    #t-shirt{
+        height: 90%;
+    }
+
+    .inner-boks3 {
+        height: 400px;
+        width: 33.3%;
+    }
+
+    #hoodie{
+        height: 90%;
+    }
+
+    .inner-boks4 {
+        height: 400px;
+        width: 33.3%;
+    }
+
+    #sweatshirt{
+        height: 90%;
+    }
+
+    .inner-boks3 {
+        height: 400px;
+    }
+    .button {
+        font-weight: 600;
+        margin-top: 10px;
+        padding: 5px;
+    }
 }
 </style>
